576b82af3e4bbf6b625dba40d93895daa872ea39
Kay <kayfelicities@outlook.com>
2018-05-30 20:53:10 +0800

    1.output pack.bin, 2.optimize ini file

--- __main__.py
+++ __main__.py
@@ -8,8 +8,8 @@ import ctypes
 import traceback
 import time
 
-VERSION = 'V1.1'
-DATE = '20180508'
+VERSION = 'V1.2'
+DATE = '20180530'
 
 if getattr(sys, 'frozen', False):
     SORTWARE_PATH = sys._MEIPASS
@@ -19,120 +19,73 @@ else:
 SPL_INI_FILE_PATH_DEFAULT = os.path.join(SORTWARE_PATH, 'NUC972DF62Y.ini')
 NUCBCH_DLL_PATH_DEFAULT = os.path.join(SORTWARE_PATH, 'nucbch.dll')
 CONFIG_FILE = r'./merge.ini'
+CONFIG_FILE_CONTENT_DEFAULT = \
+r'''# SP6 Burn Files Merge
 
+[outfile]
+# ECC位数，支持4或8
+ecc_bit = 4
+
+# 烧片文件
+burn_file = ./flash.bin
+
+# NuWriter烧写文件
+pack_file = ./pack.bin
+
+# 输入文件
+# type: data或uboot, uboot文件可用spl_ini_path项自定义spl头配置文件
+# offset:偏移位置，支持单位B/K/M/G，不带单位默认B
+[file1]
+type = uboot
+path = ./u-boot-spl.bin
+offset = 0
+spl_ini_path = 
+
+[file2]
+type = data
+path = ./u-boot.bin
+offset = 128K
+
+[file3]
+type = data
+path = ./uImage
+offset = 1M
+
+[file4]
+type = data
+path = ./cramfs.img
+offset = 3M
+'''
 class ConfigClass():
     """merge config"""
     def __init__(self):
-        self.config = configparser.ConfigParser()
         if not os.path.isfile(CONFIG_FILE):
             print('config file not found, create new.')
-            with open(CONFIG_FILE, 'w') as _: pass
-        self.config['outfile'] = {}
-        self.config['file1'] = {}
-        self.config['file2'] = {}
-        self.config['file3'] = {}
-        self.config['file4'] = {}
-        self.config.read(CONFIG_FILE)
+            with open(CONFIG_FILE, 'w', encoding='utf-8') as new_file:
+                new_file.write(CONFIG_FILE_CONTENT_DEFAULT)
+        self.config = configparser.ConfigParser()
+        self.config.read(CONFIG_FILE, encoding='utf-8-sig')
 
-        if not self.config.has_option('outfile', 'ecc_bit'):
-            self.config['outfile']['ecc_bit'] = '4'
-        if not self.config.has_option('outfile', 'merge_file'):
-            self.config['outfile']['merge_file'] = './flash.bin'
-        if not self.config.has_option('outfile', 'file1'):
-            self.config['outfile']['file1'] = ''
-        if not self.config.has_option('outfile', 'file2'):
-            self.config['outfile']['file2'] = ''
-        if not self.config.has_option('outfile', 'file3'):
-            self.config['outfile']['file3'] = ''
-        if not self.config.has_option('outfile', 'file4'):
-            self.config['outfile']['file4'] = ''
-
-        if not self.config.has_option('file1', 'path'):
-            self.config['file1']['path'] = './u-boot-spl.bin'
-        if not self.config.has_option('file1', 'offset'):
-            self.config['file1']['offset'] = '0'
-        if not self.config.has_option('file1', 'maxlen'):
-            self.config['file1']['end_offset'] = '128K'
-        if not self.config.has_option('file1', 'head_ini_path'):
-            self.config['file1']['head_ini_path'] = ''
-
-        if not self.config.has_option('file2', 'path'):
-            self.config['file2']['path'] = './u-boot.bin'
-        if not self.config.has_option('file2', 'offset'):
-            self.config['file2']['offset'] = '128K'
-        if not self.config.has_option('file2', 'maxlen'):
-            self.config['file2']['end_offset'] = '1M'
-
-        if not self.config.has_option('file3', 'path'):
-            self.config['file3']['path'] = './uImage'
-        if not self.config.has_option('file3', 'offset'):
-            self.config['file3']['offset'] = '1M'
-        if not self.config.has_option('file3', 'maxlen'):
-            self.config['file3']['end_offset'] = '3M'
-
-        if not self.config.has_option('file4', 'path'):
-            self.config['file4']['path'] = './cramfs.img'
-        if not self.config.has_option('file4', 'offset'):
-            self.config['file4']['offset'] = '3M'
-        if not self.config.has_option('file4', 'maxlen'):
-            self.config['file4']['end_offset'] = '10M'
-
-        with open(CONFIG_FILE, 'w') as configfile:
-            self.config.write(configfile)
-        self.chk_config()
-
-    def chk_config(self):
+    def chk_config(self):   #todo: 检查文件偏移是否合法，是否会覆盖
         """chk config"""
+        if not self.config.has_section('outfile')\
+            or not self.config.has_section('file1'):
+            raise Exception('config file err, please delete it and try again.')
         ecc_bit = self.config['outfile']['ecc_bit']
         if not (ecc_bit.isdigit() and int(ecc_bit) in [4, 8]):
-            print('ecc bit[%s] invalid.'%self.config['outfile']['ecc_bit'])
-            os.system('pause')
-            os._exit()
+            raise Exception('ecc bit[%s] invalid.'%self.config['outfile']['ecc_bit'])
 
-    def get_ecc_bit(self):
-        """get_ecc_bit"""
-        return int(self.config['outfile']['ecc_bit'])
-            
-    def get_out_merge_file_path(self):
-        """get_out_merge_file_path"""
-        path = self.config['outfile']['merge_file'].replace(' ', '').strip()
-        return os.path.join(path)
+    def outfile_cfg(self, key):
+        """get outfile cfg"""
+        return self.config['outfile'].get(key)
 
-    def get_out_file_path(self, file_no):
-        """get_out_file_path"""
-        path = self.config['outfile']['file%d'%file_no].replace(' ', '').strip()
-        return os.path.join(path)
-
-    def get_spl_ini_path(self):
-        """get_spl_ini_path"""
-        path = self.config['file1']['head_ini_path'].replace(' ', '').strip()
-        return os.path.join(path)
-
-    def get_file_path(self, file_no):
-        """get_file_path"""
-        path = self.config['file%d'%file_no]['path'].replace(' ', '').strip()
-        return os.path.join(path)
-
-    def __get_file_offset(self, file_no, offset_type):
-        """__get_file_offset"""
-        offset_str = self.config['file%d'%file_no][offset_type].replace(' ', '').strip()
-        if offset_str[-1] in ['b', 'B']:
-            return int(offset_str[:-1])
-        if offset_str[-1] in ['k', 'K']:
-            return int(offset_str[:-1]) * 1024
-        if offset_str[-1] in ['m', 'M']:
-            return int(offset_str[:-1]) * 1024 * 1024
-        if offset_str[-1] in ['g', 'G']:
-            return int(offset_str[:-1]) * 1024 * 1024 * 1024
-        return int(offset_str)
-
-    def get_file_offset(self, file_no):
-        """get_file_offset"""
-        return self.__get_file_offset(file_no, 'offset')
-
-    def get_file_end_offset(self, file_no):
-        """get_file_end_offset"""
-        return self.__get_file_offset(file_no, 'end_offset')
+    def infile_cfg(self, file_no, key):
+        """get infile cfg"""
+        section = 'file' + str(file_no)
+        if self.config.has_section(section):
+            return self.config[section].get(key)
+        else:
+            return None
 
 CONFIG = ConfigClass()
 
@@ -141,7 +94,7 @@ class EccClass():
     """get ecc"""
     def __init__(self):
         self.dll = ctypes.CDLL(NUCBCH_DLL_PATH_DEFAULT)
-        self.ecc_bit = CONFIG.get_ecc_bit()
+        self.ecc_bit = int(CONFIG.outfile_cfg('ecc_bit'))
         if self.ecc_bit not in [4, 8]:
             raise Exception('ecc bit[%d] invalid.'%self.ecc_bit)
         # print('ecc type: %dbit'%self.ecc_bit)
@@ -159,7 +112,7 @@ class EccClass():
             ecc_section += ctypes.string_at(ecc_obj, 8 if self.ecc_bit == 4 else 15)
         return bytestring_2048 + ecc_section
 
-def get_spl_head(spl_file_path):
+def get_spl_head(spl_file_path, spl_ini_file_path):
     """file head"""
     boot_code_marker = b'\x20TVN'
     execute_address = 0x200.to_bytes(4, 'little') # 固定0x200
@@ -171,7 +124,6 @@ def get_spl_head(spl_file_path):
 
     counter = 0
     ddr_content = b''
-    spl_ini_file_path = CONFIG.get_spl_ini_path()
     if not spl_ini_file_path:
         print('use default spl ini file')
         spl_ini_file_path = SPL_INI_FILE_PATH_DEFAULT
@@ -192,69 +144,129 @@ def get_spl_head(spl_file_path):
         for _ in range(dummy_num):
             ddr_content += b'\x00'*8
     ddr_counter = counter.to_bytes(4, 'little')
-
-    ret = boot_code_marker + execute_address + image_size + decrypt_address\
+    spl_head = boot_code_marker + execute_address + image_size + decrypt_address\
             + ddr_initial_marker + ddr_counter + ddr_content
-    return ret
+    return spl_head
 
-def merge_file(file_no, merge_file_handle):
+
+def get_offset(offset_str):
+    """get_offset"""
+    offset_str = offset_str.replace(' ', '').strip()
+    if offset_str[-1] in ['b', 'B']:
+        return int(offset_str[:-1])
+    if offset_str[-1] in ['k', 'K']:
+        return int(offset_str[:-1]) * 1024
+    if offset_str[-1] in ['m', 'M']:
+        return int(offset_str[:-1]) * 1024 * 1024
+    if offset_str[-1] in ['g', 'G']:
+        return int(offset_str[:-1]) * 1024 * 1024 * 1024
+    return int(offset_str)
+
+def merge_burn_file(infile_no, w_to_file_h):
     """merge file"""
     ecc = EccClass()
 
-    file_path = CONFIG.get_file_path(file_no)
-    file_offset = CONFIG.get_file_offset(file_no)
-    file_end_offset = CONFIG.get_file_end_offset(file_no)
-    file_out_path = CONFIG.get_out_file_path(file_no)
-    out_file = open(file_out_path, 'wb') if file_out_path else None
-    print('file{no}: {path}, offset: {offset}, end: {end}, out file: {out}'\
-            .format(no=file_no, path=file_path, offset=file_offset, end=file_end_offset, out=file_out_path))
-    if not file_path or not os.path.isfile(file_path):
-        if out_file: out_file.close()
-        raise Exception('file{no} not exist, merge abort.'.format(no=file_no))
-    if merge_file_handle: merge_file_handle.seek(file_offset)
+    infile_path = CONFIG.infile_cfg(infile_no, 'path')
+    infile_offset = get_offset(CONFIG.infile_cfg(infile_no, 'offset'))
+    infile_type = CONFIG.infile_cfg(infile_no, 'type').lower().strip()
+    print('burn:infile{no}({path}), type: {type}, offset: {offset}'\
+            .format(no=infile_no, path=infile_path, type=infile_type, offset=infile_offset))
+    if not infile_path or not os.path.isfile(infile_path):
+        raise Exception('file{no} not exist, merge abort.'.format(no=infile_no))
 
-    with open(file_path, 'rb') as in_file:
-        write_byte = 0
-        if file_no == 1:
-            spl_head = get_spl_head(file_path)
+    w_file_pos = w_to_file_h.tell()
+    for _ in range(w_file_pos, infile_offset):
+        w_to_file_h.write(b'\xff')
+    print('fill ', w_file_pos, '~', infile_offset)
+
+    with open(infile_path, 'rb') as in_file:
+        if infile_type == 'uboot':
+            spl_head = get_spl_head(infile_path, CONFIG.infile_cfg(infile_no, 'spl_ini_path'))
             page_content = spl_head + in_file.read(2048 - len(spl_head))
-        else:
+        elif infile_type == 'data':
             page_content = in_file.read(2048)
+        else:
+            raise Exception('file{no} type {type} invalid, merge abort.'\
+                .format(no=infile_no, type=infile_type))
         while page_content:
             page_to_write = ecc.get_page(page_content)
-            write_byte += len(page_to_write)
-            if write_byte > file_end_offset - file_offset:
-                raise Exception('file{no} write overflow.'.format(no=file_no))
-            if out_file: out_file.write(page_to_write)
-            if merge_file_handle: merge_file_handle.write(page_to_write)
+            w_to_file_h.write(page_to_write)
             page_content = in_file.read(2048)
-        for _ in range(write_byte, file_end_offset - file_offset):
-            if merge_file_handle: merge_file_handle.write(b'\xff')
-    if out_file: out_file.close()
+
+
+def merge_pack_file(infile_no, w_to_file_h):
+    """merge pack file"""
+    infile_path = CONFIG.infile_cfg(infile_no, 'path')
+    infile_offset = get_offset(CONFIG.infile_cfg(infile_no, 'offset'))
+    infile_type = CONFIG.infile_cfg(infile_no, 'type').lower().strip()
+    print('pack:infile{no}({path}), type: {type}, offset: {offset}'\
+            .format(no=infile_no, path=infile_path, type=infile_type, offset=infile_offset))
+    if not infile_path or not os.path.isfile(infile_path):
+        raise Exception('file{no} not exist, merge abort.'.format(no=infile_no))
+
+    with open(infile_path, 'rb') as in_file:
+        if infile_type == 'uboot':
+            spl_head = get_spl_head(infile_path, CONFIG.infile_cfg(infile_no, 'spl_ini_path'))
+            page_content = spl_head + in_file.read()
+            infile_type_no = 2
+        elif infile_type == 'data':
+            page_content = in_file.read()
+            infile_type_no = 0
+        else:
+            raise Exception('file{no} type {type} invalid, merge abort.'\
+                .format(no=infile_no, type=infile_type))
+        infile_len = len(page_content)
+        w_to_file_h.write(infile_len.to_bytes(4, 'little'))
+        w_to_file_h.write(infile_offset.to_bytes(4, 'little'))
+        w_to_file_h.write(infile_type_no.to_bytes(4, 'little'))
+        w_to_file_h.write(b'\xff'*4)
+        w_to_file_h.write(page_content)
 
 def main():
     """main"""
-    out_merge_file_path = CONFIG.get_out_merge_file_path()
-    print('ECC type: %dbit'%CONFIG.get_ecc_bit())
-    print('out merge file:', out_merge_file_path)
+    out_burn_file_path = CONFIG.outfile_cfg('burn_file')
+    out_pack_file_path = CONFIG.outfile_cfg('pack_file')
+    print('ECC {bit}bit'.format(bit=CONFIG.outfile_cfg('ecc_bit')))
+    print('out burn file:', out_burn_file_path)
+    print('out pack file:', out_pack_file_path)
 
-    outfile = open(out_merge_file_path, 'wb') if out_merge_file_path else None
-    for cnt in range(1, 5):
-        merge_file(cnt, outfile)
-    if outfile: outfile.close()
-    print('success, out merge file:', out_merge_file_path)
+    burnfile = open(out_burn_file_path, 'wb') if out_burn_file_path else None
+    packfile = open(out_pack_file_path, 'wb') if out_pack_file_path else None
+
+
+    if packfile:
+        infile_num = 0
+        infile_size_sum = 0
+        for cnt in range(1, 10):
+            if not CONFIG.infile_cfg(cnt, 'path'):
+                break
+            infile_num += 1
+            infile_size_sum += (os.path.getsize(CONFIG.infile_cfg(cnt, 'path')) + 64*1024 - 1)\
+                                 // (64*1024) * (64*1024) # 64k对齐
+        pack_action = 5
+        packfile.write(pack_action.to_bytes(4, 'little'))
+        packfile.write(infile_size_sum.to_bytes(4, 'little'))
+        packfile.write(infile_num.to_bytes(4, 'little'))
+        packfile.write(b'\xff'*4)
+    for cnt in range(1, 10):
+        if not CONFIG.infile_cfg(cnt, 'path'):
+            break
+        if burnfile: merge_burn_file(cnt, burnfile)
+        if packfile: merge_pack_file(cnt, packfile)
+    if burnfile: burnfile.close()
+    if packfile: packfile.close()
+    print('success.')
 
 
 def del_outfile():
     """delete outfile"""
     try:
-        outfile_path = CONFIG.get_out_merge_file_path()
-        if os.path.isfile(outfile_path):
-            os.remove(outfile_path)
-        for cnt in range(1, 5):
-            outfile_path = CONFIG.get_out_file_path(cnt)
-            if os.path.isfile(outfile_path):
-                os.remove(outfile_path)
+        out_burn_file_path = CONFIG.outfile_cfg('burn_file')
+        out_pack_file_path = CONFIG.outfile_cfg('pack_file')
+        if os.path.isfile(out_burn_file_path):
+            os.remove(out_burn_file_path)
+        if os.path.isfile(out_pack_file_path):
+            os.remove(out_pack_file_path)
     except Exception:
         print('outfile del failed.')
 
